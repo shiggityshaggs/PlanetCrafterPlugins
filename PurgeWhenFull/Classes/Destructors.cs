37456243b129f4f3abda8af378323fbf7e20eb3a
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2023-02-28 08:03:53 +0100

    namespace

--- PurgeWhenFull/Classes/Destructors.cs
+++ PurgeWhenFull/Classes/Destructors.cs
@@ -2,7 +2,7 @@
 using SpaceCraft;
 using System.Linq;
 
-namespace PurgeWhenFull
+namespace SSPCP
 {
     internal class Destructors
     {
--- PurgeWhenFull/Classes/Purger.cs
+++ PurgeWhenFull/Classes/Purger.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace PurgeWhenFull
+namespace SSPCP
 {
     internal class Purger
     {
