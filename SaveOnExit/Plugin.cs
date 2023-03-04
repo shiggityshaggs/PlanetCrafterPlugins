a577cac28d4490578470acfc44a058efc85ba7a6
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2023-03-05 00:11:16 +0100

    Initial

---
+++ SaveOnExit/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SpaceCraft;
+
+namespace SSPCP_SaveOnExit
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            Harmony harmony = new("SSPCP_SaveOnExit");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(UiWindowPause), "OnQuit")]
+        private class SaveOnExit
+        {
+            private static void Prefix(UiWindowPause __instance)
+            {
+                __instance.OnSave();
+            }
+        }
+    }
+}
